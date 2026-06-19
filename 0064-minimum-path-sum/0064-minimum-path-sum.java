1824f7dc7054f793f178af192ca96ff2378a3d95
Aditya Gupta <adityag7800@gmail.com>
2026-06-20 00:02:53 +0530

    Time: 1 ms (99.93%), Space: 50.4 MB (37.57%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,37 +2,23 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int prev[]=new int[n];
-
-        for(int i=0;i<m;i++){
-            int temp[]=new int[n];
-            for(int j=0;j<n;j++){
-                // Base condition 
-                if(i==0 && j==0) temp[0]=grid[0][0];
-                else{
-                    int up=(int)1e9;
-                    int left=(int)1e9;
-                    if(i>0) up=prev[j];
-                    if(j>0) left=temp[j-1];
-                    temp[j]=grid[i][j]+Math.min(up,left);
-                }
-            }
-            prev=temp;
+        int dp[][]=new int[m][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        return prev[n-1];
+
+        int ans=helper(grid,m-1,n-1,dp);
+        return dp[m-1][n-1];
     }
 
-// !!...Memoization...!!
-
-
-    // public int helper(int[][] arr,int i,int j,int dp[][]){
-    //     if(i==0 && j==0) return dp[i][j]=arr[0][0];
-    //     if(i<0 || j<0) return dp[i][j]=(int)1e9;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     int up=Integer.MAX_VALUE;
-    //     int left=Integer.MAX_VALUE;
-    //     if(i>0) up=helper(arr,i-1,j,dp);
-    //     if(j>0) left=helper(arr,i,j-1,dp);
-    //     return dp[i][j]=arr[i][j]+Math.min(up,left);
-    // }
+    public int helper(int[][] arr,int i,int j,int dp[][]){
+        if(i==0 && j==0) return dp[i][j]=arr[0][0];
+        if(i<0 || j<0) return dp[i][j]=(int)1e9;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=Integer.MAX_VALUE;
+        int left=Integer.MAX_VALUE;
+        if(i>0) up=helper(arr,i-1,j,dp);
+        if(j>0) left=helper(arr,i,j-1,dp);
+        return dp[i][j]=arr[i][j]+Math.min(up,left);
+    }
 }
