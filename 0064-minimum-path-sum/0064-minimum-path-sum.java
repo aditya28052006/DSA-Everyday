3f2397f7aafce6ba8fcc6d6390de9c889b8751f3
Aditya Gupta <adityag7800@gmail.com>
2026-06-19 23:55:42 +0530

    Time: 6 ms (10.65%), Space: 50.2 MB (54.9%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,22 +3,31 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int dp[][]=new int[m][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
 
-        int ans=helper(grid,m-1,n-1,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                // Base condition 
+                if(i==0 && j==0) dp[0][0]=grid[0][0];
+                else{
+                    int up=(int)1e9;
+                    int left=(int)1e9;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=grid[i][j]+Math.min(up,left);
+                }
+            }
+        }
         return dp[m-1][n-1];
     }
 
-    public int helper(int[][] arr,int i,int j,int dp[][]){
-        if(i==0 && j==0) return dp[i][j]=arr[0][0];
-        if(i<0 || j<0) return dp[i][j]=(int)1e9;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=Integer.MAX_VALUE;
-        int left=Integer.MAX_VALUE;
-        if(i>0) up=helper(arr,i-1,j,dp);
-        if(j>0) left=helper(arr,i,j-1,dp);
-        return dp[i][j]=arr[i][j]+Math.min(up,left);
-    }
+    // public int helper(int[][] arr,int i,int j,int dp[][]){
+    //     if(i==0 && j==0) return dp[i][j]=arr[0][0];
+    //     if(i<0 || j<0) return dp[i][j]=(int)1e9;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int up=Integer.MAX_VALUE;
+    //     int left=Integer.MAX_VALUE;
+    //     if(i>0) up=helper(arr,i-1,j,dp);
+    //     if(j>0) left=helper(arr,i,j-1,dp);
+    //     return dp[i][j]=arr[i][j]+Math.min(up,left);
+    // }
 }
