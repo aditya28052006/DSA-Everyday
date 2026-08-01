0dab2e894b583662262eb43ca57af29fa0654808
Aditya Gupta <adityag7800@gmail.com>
2026-08-01 15:56:48 +0530

    Time: 5 ms (98.88%), Space: 46.8 MB (25.86%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,36 +1,30 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int[] f:flights){
-            adj.get(f[0]).add(new int[]{f[1],f[2]});
-        }
-        int[] dist=new int[n];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[src]=0;
-        Queue<int[]> q=new LinkedList<>();
-        q.offer(new int[]{src,0}); // node , cost
-        int stops=0;
+        int[] prices = new int[n];
+        Arrays.fill(prices, Integer.MAX_VALUE);
+        prices[src] = 0;
 
-        while(!q.isEmpty()&&stops<=k){
-            int size=q.size();
-            while(size-->0){
-                int curr[]=q.poll();
-                int node=curr[0];
-                int cost=curr[1];
-                for(int[] nei:adj.get(node)){
-                    int next=nei[0];
-                    int price=nei[1];
-                    if(cost+price<dist[next]){
-                        dist[next]=cost+price;
-                        q.offer(new int[]{next,dist[next]});
-                    }
+        for (int i = 0; i <= k; i++) {
+
+            int[] tempPrices = prices.clone();
+
+            for (int[] flight : flights) {
+
+                int s = flight[0];
+                int d = flight[1];
+                int p = flight[2];
+
+                if (prices[s] == Integer.MAX_VALUE)
+                    continue;
+
+                if (prices[s] + p < tempPrices[d]) {
+                    tempPrices[d] = prices[s] + p;
                 }
             }
-            stops++;
+
+            prices = tempPrices;
         }
-        return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
+
+        return prices[dst] == Integer.MAX_VALUE ? -1 : prices[dst];
     }
 }
