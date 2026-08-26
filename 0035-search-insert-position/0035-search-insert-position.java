a2ad87becc594b2febabf80807081e1b051457cb
Aditya Gupta <adityag7800@gmail.com>
2026-08-26 23:34:24 +0530

    Time: 0 ms (100%), Space: 44.6 MB (67.02%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int l=0;
+        int h=arr.length-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(arr[mid]==target) return mid;
+            if(target>arr[mid]) l=mid+1;
+            else h=mid-1;
+        }
+        return l;
+    }
+}
